6f9b4f206739427654823454ef8e959a638d7ee0
surajpshenoy <surajpshenoy@gmail.com>
2021-07-23 17:23:06 +1000

    added the food data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from "react"
-
+import React, {useState} from "react"
+import data from "./data"
 
 function App() {
+
+  const [foodItems, setFoodItems] = useState(data)
   return (
     <div className="App">
       <h3>Testing the Food Selector page</h3>
+      {console.log(foodItems)}
     </div>
   );
 }
